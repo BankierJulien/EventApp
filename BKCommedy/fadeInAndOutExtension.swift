b03d0f51a4a96fe837b5fc255228630ca95eb7ab
BankierJulien <julienbankiernyc@gmail.com>
2017-06-19 21:29:46 -0400

    added animation to home view

---
+++ BKCommedy/fadeInAndOutExtension.swift
@@ -0,0 +1,21 @@
+//
+//  fadeInAndOutExtension.swift
+//  BKCommedy
+//
+//  Created by Julien Bankier on 6/19/17.
+//  Copyright © 2017 Julien Bankier. All rights reserved.
+//
+
+import UIKit
+
+class fadeInAndOutExtension: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
